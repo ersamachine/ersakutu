0e2de3523959d0af5f3782ab74ae195b3dfef4ca
ersamachine <easyboxmachine@gmail.com>
2026-06-25 10:53:00 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// ERSA KUTU — Apps Script Web App URL'ini buraya yapıştır (dağıttıktan sonra .../exec ile biten adres)
-window.KUTU_API_URL = "BURAYA_WEB_APP_URL";
+// ERSA KUTU — Apps Script Web App URL
+window.KUTU_API_URL = "https://script.google.com/macros/s/AKfycby5niQ9ZPfxZkjJl8dYiflnxn6ZjDDChKjnFqp_DIbJWzDPv51OmJvNtbSzdI19tPQkkg/exec";
