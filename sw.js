c2827e5f4093d84e5c4b311021ba04f351b7d74b
ersamachine <easyboxmachine@gmail.com>
2026-07-04 13:21:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const C="ersa-kutu-v8";
+﻿const C="ersa-kutu-v9";
 const A=["./","index.html","manifest.webmanifest","config.js"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A).catch(()=>{})));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
